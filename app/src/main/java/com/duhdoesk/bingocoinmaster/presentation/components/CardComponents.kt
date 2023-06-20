1dd951eccfa53fe4cd51bdaad6a800c98914fba6
duhdoesk <dev.edusc@gmail.com>
2023-06-19 21:02:08 -0300

    coil implementation for image loading

--- app/src/main/java/com/duhdoesk/bingocoinmaster/presentation/components/CardComponents.kt
+++ app/src/main/java/com/duhdoesk/bingocoinmaster/presentation/components/CardComponents.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,12 +27,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.duhdoesk.bingocoinmaster.R
 import com.duhdoesk.bingocoinmaster.model.Card
 import com.duhdoesk.bingocoinmaster.presentation.drawer.DrawState
@@ -141,12 +146,18 @@ fun BingoStone(
             shape = RoundedCornerShape(12.dp),
             colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondary)
         ) {
-            Box(contentAlignment = Alignment.BottomStart) {
-                Image(
-                    painter = painterResource(id = R.drawable.placeholder),
-                    contentScale = ContentScale.Inside,
+            Box(
+                contentAlignment = Alignment.BottomStart,
+                modifier = Modifier.height(140.dp)
+            ) {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(card.picture)
+                        .crossfade(true)
+                        .build(),
                     contentDescription = "Stone Picture",
-                    modifier = Modifier.fillMaxWidth()
+                    placeholder = painterResource(id = R.drawable.placeholder),
+                    contentScale = ContentScale.Crop
                 )
 
                 Box(
